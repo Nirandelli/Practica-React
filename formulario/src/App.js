c65f3783c9759a6c5ca41390509701c0fef7f1d5
delli patricio <delli.patricio.mayo@gmail.com>
2017-07-20 18:52:51 -0500

    Version-0.2

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './component/Button'
+import Input from './component/Input'
 
 class App extends Component {
+
   render() {
-    return <Button/>
+    return (
+              <div>
+                <Input/>
+                <Button/>
+              </div>
+    );
   }
 }
 
